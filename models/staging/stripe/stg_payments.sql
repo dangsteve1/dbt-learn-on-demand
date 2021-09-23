73063f08078f0c9c39b3fde8aa0eae1182d87e36
Steve Dang <sdang@clearcover.com>
2021-09-23 03:36:07 +0000

    macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ select
     paymentmethod as payment_method,
     status,
 
-    amount / 100 as amount,
+   {{ cents_to_dollars('amount') }} as amount,
     created as created_at
 
 from raw.stripe.payment
